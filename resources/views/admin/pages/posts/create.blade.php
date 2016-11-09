74c765f10d0153651ee115d7dbc5cc6e897f2eba
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-11-10 00:19:41 +0100

    Add new post

--- resources/views/admin/pages/posts/create.blade.php
+++ resources/views/admin/pages/posts/create.blade.php
@@ -6,8 +6,53 @@
 
 @section('content')
     
-    <div class="container m-t-2">
-        New post!
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+                <h2>New post</h2>
+                
+                <div class="margin-top-lg">
+                    {!! Form::open(['route' => ['admin.posts.store']]) !!}
+                    
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            Please check your input and correct any errors.
+                        </div>
+                    @endif
+                    
+                    <div class="form-group {{ Html::highlightOnError('title') }}">
+                        <label for="title" class="form-control-label">Title</label>
+                        {!! Form::text('title', null, ['id' => 'title', 'class' => 'form-control', 'maxlength' => 80, 'required']) !!}
+                        {!! Html::error('title') !!}
+                    </div>
+                    
+                    <div class="form-group {{ Html::highlightOnError('slug') }}">
+                        <label for="slug" class="form-control-label">Slug</label>
+                        {!! Form::text('slug', null, ['id' => 'slug', 'class' => 'form-control', 'maxlength' => 80, 'required']) !!}
+                        {!! Html::error('slug') !!}
+                    </div>
+                    
+                    {{--
+                    <div class="form-group form-optional {{ Html::highlightOnError('end_at') }}">
+                        <label for="end_at" class="form-control-label">Deadline</label>
+                        {!! Form::date('end_at', null, ['id' => 'end_at', 'class' => 'form-control', 'maxlength' => 9]) !!}
+                        {!! Html::error('end_at') !!}
+                    </div>
+                    --}}
+                    
+                    <div class="form-group {{ Html::highlightOnError('body') }}">
+                        {!! Form::textarea('body', null, ['id' => 'reason', 'class' => 'form-control', 'maxlength' => 16383, 'rows' => 20]) !!}
+                        {!! Html::error('body') !!}
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary">Create</button>
+                    
+                    {!! Form::close() !!}
+                </div>
+            
+            </div>
+        </div>
     </div>
 
 @stop
--- resources/views/admin/partials/navigation.blade.php
+++ resources/views/admin/partials/navigation.blade.php
@@ -1,48 +1,24 @@
-<nav class="navbar navbar-full navbar-light bg-faded">
+<nav class="navbar navbar-full navbar-light bg-faded margin-bottom-lg">
     <div class="container">
         <div class="row">
             <div class="col-md-8 flex flex-items-md-middle">
-                {{--<ul class="nav nav-pills">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Active</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Separated link</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>--}}
                 <ul class="nav navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="{{ route('admin.posts.index') }}" title="Home">Posts <span class="tag tag-default">11</span> <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="{{ route('admin.posts.index') }}" title="Posts">Posts <span class="tag tag-default">11</span> <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" title="Home">Media</a>
+                        <a class="nav-link" href="#" title="Media">Media</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" title="Settings">Settings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" title="Account">Account</a>
                     </li>
                 </ul>
             </div>
-            <div id="nav-user" class="col-md-4 flex flex-items-md-right">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" title="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img src="https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg" alt="Your avatar" width="30" class="user-avatar img-rounded" /> Sebastiaan Luca
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <a class="dropdown-item" href="#" title="End your session">Log out</a>
-                        </div>
-                    </li>
-                </ul>
+            <div class="col-md-4 text-md-right">
+                <a class="btn btn-primary" href="{{ route('admin.posts.create') }}" title="Write a new post">Write</a>
             </div>
         </div>
     </div>
