be204d2beab9f3b92a337bbaf5f96631fdc68aca
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-11-14 20:58:51 +0100

    Fix log out link spacing

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -4,7 +4,7 @@
             
             <nav class="nav nav-inline">
                 {!! Form::open(['route' => 'blog::admin.auth.logout.post']) !!}
-                <button type="submit" class="btn btn-link nav-link">Log out</button>
+                <button type="submit" class="btn-link nav-link">Log out</button>
                 {!! Form::close() !!}
             </nav>
         
