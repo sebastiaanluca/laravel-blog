f8f37eaebb1c1346a36f761e5765dedfecf208d6
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-11-14 19:27:37 +0100

    Add logout button

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="container margin-top-xl">
+    <div class="row">
+        <div class="col-md-12">
+            
+            <nav class="nav nav-inline">
+                {!! Form::open(['route' => 'blog::admin.auth.logout.post']) !!}
+                <button type="submit" class="btn btn-link nav-link">Log out</button>
+                {!! Form::close() !!}
+            </nav>
+        
+        </div>
+    </div>
+</footer>
