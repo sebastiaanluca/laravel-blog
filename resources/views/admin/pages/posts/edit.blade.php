c9725942d66ad85d6142e78f839ac0ab16249b22
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-11-14 20:37:53 +0100

    Fix datetime input

--- resources/views/admin/pages/posts/edit.blade.php
+++ resources/views/admin/pages/posts/edit.blade.php
@@ -41,8 +41,7 @@
                         <div class="col-md-3">
                             <div class="form-group {{ Html::highlightOnError('published_at') }}">
                                 <label for="published_at" class="form-control-label">Publish date and time</label>
-                                {{--TODO: add datetime + datepicker datetime option--}}
-                                {!! Form::text('published_at', \Carbon\Carbon::now(), ['id' => 'published_at', 'class' => 'form-control', 'maxlength' => 9]) !!}
+                                {!! Form::date('published_at', \Carbon\Carbon::now(), ['id' => 'published_at', 'class' => 'form-control', 'maxlength' => 9]) !!}
                                 {!! Html::error('published_at') !!}
                             </div>
                         </div>
