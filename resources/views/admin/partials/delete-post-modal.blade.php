988141fc230e2dc9d26105b334ba3922e19837f5
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-11-15 01:27:39 +0100

    Tweak post delete modal

--- resources/views/admin/partials/delete-post-modal.blade.php
+++ resources/views/admin/partials/delete-post-modal.blade.php
@@ -13,7 +13,7 @@
             </div>
             <div class="modal-body">
                 <p>Are you sure you want to delete this post? You will not be able to recover it in the future.</p>
-                <p><span class="fa fa-info-circle margin-right-xxs"></span>Considering unpublishing it by marking it as a draft instead.</p>
+                <p class="text-muted">Consider unpublishing it by marking it as a draft instead.</p>
             </div>
             <div class="modal-footer">
                 <button type="submit" class="btn btn-danger btn-block">Delete</button>
