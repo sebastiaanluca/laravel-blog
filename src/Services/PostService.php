e096f9f26cba02bc7c1d0c55bd812e4755f1f254
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-11-15 02:15:59 +0100

    Update post service

--- src/Services/PostService.php
+++ src/Services/PostService.php
@@ -43,4 +43,36 @@ class PostService
         
         return $this->posts->published()->orderChronologically()->get($columns);
     }
+    
+    /**
+     * Get a published post by its slug.
+     *
+     * @param string $slug
+     *
+     * @return \SebastiaanLuca\Blog\Models\Post
+     */
+    public function getPublishedPostBySlug(string $slug) : Post
+    {
+        // TODO: parse MarkDown
+        
+        $post = $this->posts->published()->where('slug', $slug)->firstOrFail();
+        
+        $post = $this->removeIntroTagFromPostBody($post);
+        
+        return $post;
+    }
+    
+    /**
+     * Remove the intro tag from the post's body.
+     *
+     * @param \SebastiaanLuca\Blog\Models\Post $post
+     *
+     * @return \SebastiaanLuca\Blog\Models\Post
+     */
+    protected function removeIntroTagFromPostBody(Post $post) : Post
+    {
+        $post->body = str_replace('[endintro]', '', $post->body);
+        
+        return $post;
+    }
 }
